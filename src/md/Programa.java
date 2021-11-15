46aa695c6966847e25b3bd660c68493787b720f5
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-15 02:52:17 -0300

    atualizar calendario com 4 horas a mais

---
+++ src/md/Programa.java
@@ -0,0 +1,25 @@
+package md;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Programa {
+
+	public static void main(String[] args)throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();//instanciar calendário 
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4); //adicionar 4 horas do dia
+		d = cal.getTime(); //atualizar hora 
+		System.out.println(sdf.format(d));
+	}
+
+}
